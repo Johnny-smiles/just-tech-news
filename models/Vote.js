439cc54c9b567bf1a9afce813e97d2c4b8fe985d
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-29 13:56:54 -0600

    created model for upvoting. passing through post data into user table.

---
+++ models/Vote.js
@@ -0,0 +1,43 @@
+//linking sequelize
+const { Model, DataTypes } = require('sequelize');
+// linking db
+const sequelize = require('../config/connection');
+
+// creating class voting. 
+class Vote extends Model {}
+
+// creating new voting instance. 
+Vote.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'vote'
+    }
+  );
+
+module.exports = Vote;
