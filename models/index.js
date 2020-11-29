df9ea8e7cd73454a63e664f471dbc3e2816299bf
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-28 23:42:45 -0600

    added routes for CRUDing users posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations. linking One user to many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// creating associations. Linking many post to one user.
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
 module.exports = { User, Post };
