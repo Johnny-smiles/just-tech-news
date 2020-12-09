eeae99602b8095e05ad47d884a1ee70bd014df68
Johnny-smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-12-08 22:37:19 -0600

    started work on up vote functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -98,15 +98,19 @@ router.get('/:id', (req, res) => {
   // endpoint for up vote.
   // PUT /api/posts/upvote
   router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
+  });
 
+  
   // update posts title
   router.put('/:id', (req, res) => {
     Post.update(
